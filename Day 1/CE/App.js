215dab4da04c078db49842ade9aa2e26783df1c3
SARAVANAN C <147690847+itzmesara@users.noreply.github.com>
2024-07-31 16:19:47 +0530

    Update App.js

--- Day 1/CE/App.js
+++ Day 1/CE/App.js
@@ -1,1 +1,17 @@
+import React from 'react'
 
+function alertMsg()
+{
+  alert('Message from Javascript alert');
+  console.log('Message to developer');
+}
+function App() {
+  return (
+    <div>
+      <h1>Let we see the output of JAVASCRIPT</h1>
+      <button onClick={alertMsg}>Click</button>
+    </div>
+  );
+}
+
+export default App;
